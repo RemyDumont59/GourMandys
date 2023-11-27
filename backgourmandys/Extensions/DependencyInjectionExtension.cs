012167c2b79100ccea9fcde9a19f7de8df0332a6
03Sly03 <slydevwebproject@outlook.fr>
2023-11-27 12:12:04 +0100

    Cake repo, extensions with depencenceis Injection added

---
+++ backgourmandys/Extensions/DependencyInjectionExtension.cs
@@ -0,0 +1,34 @@
+﻿using backgourmandys.Datas;
+using backgourmandys.Models;
+using backgourmandys.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace backgourmandys.Extensions;
+
+public static class DependencyInjectionExtension
+{
+    public static void InjectDependancies(this WebApplicationBuilder builder)
+        {
+
+            builder.Services.AddControllers();
+            
+            builder.AddDatabase();
+
+            builder.AddRepositories();
+
+        }
+
+        private static void AddDatabase(this WebApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+        }
+
+        private static void AddRepositories(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<IRepository<Cake>, CakeRepository>();
+        }
+}
