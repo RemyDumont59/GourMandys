9aeb75df17a0692364a893216cc0c4afac940d24
Remy DUMONT <dumont.remy.59@gmail.com>
2023-11-27 11:57:53 +0100

    Add-Models Cake && Enum

--- backgourmandys/Models/Cake.cs
+++ backgourmandys/Models/Cake.cs
@@ -1,6 +1,26 @@
-﻿namespace backgourmandys.Models
+﻿using backgourmandys.Enum;
+using System.ComponentModel.DataAnnotations;
+
+namespace backgourmandys.Models
 {
     public class Cake
     {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        [Required]
+        public Category Category { get; set; }
+        public Flavour Flavour { get; set; }
+        [Required]
+        public List<string>? PicturePath { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        public int Pieces { get; set; }
+        public string? Size { get; set; }
+        public int MinimalQuantity { get; set; }
+        public int Lot { get; set; }
+        public bool IsNumberOrLetterCake { get; set; }
     }
 }
