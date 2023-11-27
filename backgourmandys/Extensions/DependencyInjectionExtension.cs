58936881ac0e621b24f8810e790b79ae127770fc
Remy DUMONT <dumont.remy.59@gmail.com>
2023-11-27 16:07:30 +0100

    Update DependencyInjectionExtension (JsonOptions -> IgnoreCycles)

--- backgourmandys/Extensions/DependencyInjectionExtension.cs
+++ backgourmandys/Extensions/DependencyInjectionExtension.cs
@@ -2,34 +2,35 @@
 using backgourmandys.Models;
 using backgourmandys.Repositories;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 namespace backgourmandys.Extensions;
 
 public static class DependencyInjectionExtension
 {
     public static void InjectDependancies(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers().AddJsonOptions(x =>
+                        x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+        builder.AddDatabase();
+
+        builder.AddRepositories();
+
+    }
+
+    private static void AddDatabase(this WebApplicationBuilder builder)
+    {
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
         {
+            options.UseSqlServer(connectionString);
+        });
+    }
 
-            builder.Services.AddControllers();
-            
-            builder.AddDatabase();
-
-            builder.AddRepositories();
-
-        }
-
-        private static void AddDatabase(this WebApplicationBuilder builder)
-        {
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            {
-                options.UseSqlServer(connectionString);
-            });
-        }
-
-        private static void AddRepositories(this WebApplicationBuilder builder)
-        {
-            builder.Services.AddScoped<IRepository<Cake>, CakeRepository>();
-            builder.Services.AddScoped<IRepository<PicturePath>, PicturePathRepository>();
-        }
+    private static void AddRepositories(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<IRepository<Cake>, CakeRepository>();
+        builder.Services.AddScoped<IRepository<PicturePath>, PicturePathRepository>();
+    }
 }
